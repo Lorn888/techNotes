687c63bc41a94c1942a47b3815dca8a72a7cf2ac
Patryk <patrykokpara@hotmail.com>
2023-03-30 22:06:58 +0100

    Refactoring

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-    'http://192.168.0.229:3007',
-    'https://www.dandylolshp.com',
-    'https://www.somelolstrona.com'
-]
+  "http://192.168.0.229:3007",
+  "https://www.dandylolshp.com",
+  "https://www.somelolstrona.com",
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
--- models/Note.js
+++ models/Note.js
@@ -1,36 +1,34 @@
-const mongoose = require('mongoose')
-const AutoIncrement = require('mongoose-sequence')(mongoose)
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const noteSchema = new mongoose.Schema(
-    {
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     text: {
-        type: String,
-        default: true
+      type: String,
+      default: true,
     },
     completed: {
-        type: Boolean,
-        default: false
-    }
-
-},
-{
-    timestamps: true
-}
-)
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 noteSchema.plugin(AutoIncrement, {
-    inc_field: 'ticket',
-    id: 'ticketNums',
-    start_seq: 500
-
-})
-module.exports = mongoose.model('Note', noteSchema)
+  inc_field: "ticket",
+  id: "ticketNums",
+  start_seq: 500,
+});
+module.exports = mongoose.model("Note", noteSchema);
